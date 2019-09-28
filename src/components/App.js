952fcf39bc55d22c94973cd8b8c12c690cf4327a
Timothy Hamilton <TimH@crossbrowsertesting.com>
2019-09-28 00:31:51 -0500

    use external hosted api

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ export class App extends React.Component {
     render() {
         return (
             <div className="wrapper">
-                <SocketContext.Provider value={io('http://192.168.0.106:3000')}>
+                <SocketContext.Provider value={io('https://partyapi.timmytango.com/')}>
                     <NowPlaying />
                     <TrackChoices />
                 </SocketContext.Provider>
